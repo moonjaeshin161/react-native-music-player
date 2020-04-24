7077c8acf3c79cbd82d5b0d94385141b170ea5af
MoonJaeShin <moonjaeshin@MoonJaeShins-iMac-Pro.local>
2020-04-24 19:26:35 +0700

    add react-native-track-player

---
+++ ios/dummy.swift
@@ -0,0 +1,8 @@
+//
+//  dummy.swift
+//  musicPlayerApp
+//
+//  Created by MoonJaeShin on 4/24/20.
+//
+
+import Foundation
--- views/musicPlayer/PlayerScreen.js
+++ views/musicPlayer/PlayerScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 const PlayerScreen = () => {
+
     return (
         <View>
             <Text>Player</Text>
