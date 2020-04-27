a30c6ed48b66005e3f3f80ac2bed0481e9b8a144
MoonJaeShin <moonjaeshin@MoonJaeShins-iMac-Pro.local>
2020-04-27 18:45:31 +0700

    add rn-get-music-files lib

---
+++ views/musicPlayer/ListMusicScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ListMusicScreen = () => {
+    return (
+        <View>
+            <Text>ListMusicScreen</Text>
+        </View>
+    )
+}
+
+export default ListMusicScreen
