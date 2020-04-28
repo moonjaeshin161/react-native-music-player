bd1056665690074098160a22c28632a65835c496
MoonJaeShin <moonjaeshin@MoonJaeShins-iMac-Pro.local>
2020-04-28 13:45:30 +0700

    add tab icon

--- navigations/AppTab.js
+++ navigations/AppTab.js
@@ -2,11 +2,33 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeStack from './HomeStack';
 import MusicPlayerStack from './MusicPlayerStack';
+import Entypo from 'react-native-vector-icons/Entypo'
 
 const Tab = createBottomTabNavigator();
 
 const AppTab = () => (
-    <Tab.Navigator >
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === 'Home') {
+                    iconName = focused
+                        ? 'home'
+                        : 'home';
+                } else if (route.name === 'MusicPlayer') {
+                    iconName = focused ? 'folder-music' : 'folder-music';
+                }
+
+                // You can return any component that you like here!
+                return <Entypo name={iconName} size={size} color={color} />;
+            },
+        })}
+        tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        }}
+    >
         <Tab.Screen name='Home' component={HomeStack} />
         <Tab.Screen name='MusicPlayer' component={MusicPlayerStack} />
     </Tab.Navigator>
--- views/main/HomeScreen.js
+++ views/main/HomeScreen.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { scale, verticalScale } from 'react-native-size-matters';
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Bài tập lớn - App nghe nhạc</Text>
+            <Text style={styles.subTitle}>Các thành viên trong nhóm</Text>
+            <View style={styles.listContainer}>
+                <Text style={styles.member}>Nguyễn Công Huynh</Text>
+                <Text style={styles.member}>Vũ Đăng Huy</Text>
+                <Text style={styles.member}>Hoàng Thái Hà</Text>
+                <Text style={styles.member}>Doãn Đoàn Đại Hùng</Text>
+                <Text style={styles.member}>Kiều Thanh Phong</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        fontSize: scale(25),
+        fontWeight: '600',
+        textAlign: 'center'
+    },
+    subTitle: {
+        fontSize: scale(22),
+        fontWeight: '400',
+        textAlign: 'center'
+    },
+    listContainer: {
+        paddingTop: verticalScale(10)
+    },
+    member: {
+        fontSize: scale(13),
+        fontWeight: '300',
+        textAlign: 'center',
+        marginBottom: scale(5)
+    }
+})
 
 export default HomeScreen
